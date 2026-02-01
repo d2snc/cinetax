c7c18223dc2e9cf72c7140269ae312def9c0cadf
DL <>
2026-01-31 23:32:14 -0300

    pixel changes

--- app/lib/fbpixel.ts
+++ app/lib/fbpixel.ts
@@ -1,19 +1,10 @@
 export const FB_PIXEL_ID = '1414284940077637';
 
-declare global {
-    interface Window {
-        fbq: any;
-    }
-}
-
 export const pageview = () => {
-    if (typeof window !== 'undefined' && window.fbq) {
-        window.fbq('track', 'PageView');
-    }
+    window.fbq("track", "PageView");
 };
 
-export const event = (name: string, options = {}) => {
-    if (typeof window !== 'undefined' && window.fbq) {
-        window.fbq('track', name, options);
-    }
+// https://developers.facebook.com/docs/facebook-pixel/advanced/
+export const event = (name: any, options = {}) => {
+    window.fbq("track", name, options);
 };
