69ec032627a1ee30ad64657f1843b3cd9d0b703d
DL <>
2026-01-31 23:51:11 -0300

    changes facebook pixel

--- app/components/FacebookPixel.tsx
+++ app/components/FacebookPixel.tsx
@@ -15,36 +15,15 @@ const FacebookPixel = () => {
     }, [pathname, loaded]);
 
     return (
-        <>
+        <div>
             <Script
                 id="fb-pixel"
+                src="/scripts/pixel.js"
                 strategy="afterInteractive"
                 onLoad={() => setLoaded(true)}
-                dangerouslySetInnerHTML={{
-                    __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${pixel.FB_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
-                }}
+                data-pixel-id={pixel.FB_PIXEL_ID}
             />
-            <noscript>
-                <img
-                    height="1"
-                    width="1"
-                    style={{ display: 'none' }}
-                    src={`https://www.facebook.com/tr?id=${pixel.FB_PIXEL_ID}&ev=PageView&noscript=1`}
-                    alt=""
-                />
-            </noscript>
-        </>
+        </div>
     );
 };
 
